9ba07bcf7705ab3712081281f43aa4324d523243
Miles Wu <miles.wu@wu-home.co.uk>
2010-01-22 02:09:44 -0500

    Browsing

--- mkv2mp4-gui.rb
+++ mkv2mp4-gui.rb
@@ -1,9 +1,30 @@
 require 'wx'
 include Wx
 
+class MyFrame < Wx::Frame
+	def initialize
+		super(nil, :title => "mkv2mp4-GUI", :pos => DEFAULT_POSITION, :size => DEFAULT_SIZE)
+		
+		panel = Panel.new(self)
+		
+		browseButton = Button.new(panel, ID_ANY, "Browse")
+		evt_button(browseButton.get_id()) {|event| browse }
+		
+		show
+	end
+	
+	def browse
+		f = FileDialog.new(nil, "Choose files","", "", "*.mkv", FD_MULTIPLE)
+		f.show_modal
+		paths = f.get_paths
+   end
+end
+
 class MinimalApp < App
    def on_init
-        Frame.new(nil, -1, "The Bare Minimum").show()
+       MyFrame.new()
    end
+   
+   
 end
 MinimalApp.new.main_loop
