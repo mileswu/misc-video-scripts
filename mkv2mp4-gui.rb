657066c313b23a2b14618624accb4a723ca46aca
Miles Wu <miles.wu@wu-home.co.uk>
2010-01-22 10:44:13 -0500

    Browsing

--- mkv2mp4-gui.rb
+++ mkv2mp4-gui.rb
@@ -1,6 +1,20 @@
 require 'wx'
 include Wx
 
+class FileList < Wx::ListCtrl
+	def initialize
+		super
+		insert_column(0, "name")
+		
+		@data = [{"name" => "a"}]
+		set_item_count 1
+	end
+	
+	def on_get_item_text(item, column)
+		@data[item][column]
+	end
+end
+
 class MyFrame < Wx::Frame
 	def initialize
 		super(nil, :title => "mkv2mp4-GUI", :pos => DEFAULT_POSITION, :size => DEFAULT_SIZE)
@@ -9,6 +23,12 @@ class MyFrame < Wx::Frame
 		
 		browseButton = Button.new(panel, ID_ANY, "Browse")
 		evt_button(browseButton.get_id()) {|event| browse }
+		
+		startButton = Button.new(panel, ID_ANY, "Start")
+		evt_button(browseButton.get_id()) {|event| start }
+		
+		listControl = FileList.new(panel, ID_ANY)
+		
 		
 		show
 	end
